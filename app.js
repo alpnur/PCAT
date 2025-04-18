569dd841b1677d358e1fd0b9c21791aea96ea76d
alpnur <nuralppp862@gmail.com>
2025-04-18 16:53:20 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+const path =require('path')
+
+const app = express();
+
+const myLogger = (req,res,next)=>{
+    console.log("Middleware Log 1")
+    next()
+}
+
+//MIDDLEWARES
+app.use(express.static('public'))
+// app.use(myLogger)
+
+
+app.get('/', (req, res) => {
+//   const photo = {
+//     id: 1,
+//     name: 'Photo Name',
+//     description: 'Photo description',
+//   };
+//   res.send(photo);
+res.sendFile(path.resolve(__dirname,'temp/index.html'))
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
